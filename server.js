c114b2d928b5ddb516c36eace3cbca7785ce4b42
John Willcocks <johnwil1@uk.ibm.com>
2017-04-06 15:46:36 +0100

    initial commit

---
+++ server.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const fs = require('fs');
+
+var app = express();
+
+app.use(express.static(__dirname + '/public'));
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('unable to append to server.log')
+    }
+  })
+
+  next();
+});
+
+app.get('/', (req, res) => {
+  //res.send('Hello Express!');
+
+  res.send({
+    name: 'John',
+    likes: [
+    'computer games',
+    'reading'
+  ]
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.send('About page');
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request',
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000!');
+});
